80381b5254694a4c6ea34bc86571819fa8c97abd
Marghrid <margarida@cmu.edu>
2025-04-04 21:56:10 -0400

    dark mode toggle button

---
+++ dark-mode-toggle.js
@@ -0,0 +1,22 @@
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-theme");
+  // const isDarkMode = document.body.classList.contains("dark-theme");
+  // localStorage.setItem("darkMode", isDarkMode);
+}
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  toggleDarkMode();
+  document.getElementById("darkmode-toggle").checked = true;
+}
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    console.log("Color scheme changed to: " + newColorScheme);
+    toggleDarkMode();
+    document.getElementById("darkmode-toggle").checked =
+      newColorScheme === "dark";
+  });
